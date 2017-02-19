329bbf1f6fb41b83bc16aec1431ddd01e538e6ff
TMTron <martin.trummer@tmtron.com>
2017-02-19 12:24:24 +0100

    added comments with questions

--- app/src/main/java/com/tmtron/rxjava2udp/UdpObservable.java
+++ app/src/main/java/com/tmtron/rxjava2udp/UdpObservable.java
@@ -29,12 +29,22 @@ class UdpObservable {
         @Override
         public void run() {
             try {
+                // we don't want to create the DatagramSocket in the constructor, because this
+                // might raise an Exception that the observer wants to handle
                 udpSocket = new DatagramSocket(portNo);
                 try {
+                    /* QUESTION 1:
+                       Do I really need to check isInterrupted() and emitter.isDisposed()?
+
+                       When the thread is interrupted an interrupted exception will
+                       be raised anyway and the emitter is being disposed (this is what
+                       caused the interruption)
+                    */
                     while (!isInterrupted() && !emitter.isDisposed()) {
                         byte[] rcvBuffer = new byte[bufferSizeInBytes];
                         DatagramPacket datagramPacket = new DatagramPacket(rcvBuffer, rcvBuffer.length);
                         udpSocket.receive(datagramPacket);
+                        // QUESTION 1a: same as QUESTION 1 above
                         if (!isInterrupted() && !emitter.isDisposed()) {
                             emitter.onNext(datagramPacket);
                         }
@@ -43,10 +53,14 @@ class UdpObservable {
                     closeUdpSocket();
                 }
             } catch (Throwable th) {
+                // the thread will only be interrupted when the observer has unsubscribed:
+                // so we need not report it
                 if (!isInterrupted()) {
                     if (!emitter.isDisposed()) {
                         emitter.onError(th);
                     } else {
+                        // QUESTION 2: is this the correct way to handle errors, when the emitter
+                        //             is already disposed?
                         RxJavaPlugins.onError(th);
                     }
                 }
@@ -62,6 +76,8 @@ class UdpObservable {
         @Override
         public void interrupt() {
             super.interrupt();
+            // QUESTION 3: this is called from an external thread, right, so
+            //             how can we correctly synchronize the access to udpSocket?
             closeUdpSocket();
         }
     }
@@ -79,6 +95,8 @@ class UdpObservable {
                     @Override
                     public void subscribe(ObservableEmitter<DatagramPacket> emitter) throws Exception {
                         final UdpThread udpThread = new UdpThread(emitter, portNo, bufferSizeInBytes);
+                        /* QUESTION 4: Is this the right way to handle unsubscription?
+                         */
                         emitter.setCancellable(new Cancellable() {
                             @Override
                             public void cancel() throws Exception {
